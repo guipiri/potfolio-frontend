9a672135226085b5e955b4c739ca34dc9423a7a4
Guilherme Oliveira <gui.soliveiras@gmail.com>
2023-01-25 11:56:52 -0300

    Corrigindo rotas: rota root é a <Home/> e todas as outras serão filhas dessa. Footer criado no mesmo aspecto que a NavBar. NavBar ajustada no centro, margem nos botões trocada para padding.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import AboutMe from './pages/AboutMe'
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import InitialAnimation from "./components/InitialAnimation";
 
 const router = createBrowserRouter([
 	{
-		path: "/",
-		element: <App />,
+		element: <Home />,
+		children: [
+			{
+				path: "/",
+				element: (
+					<InitialAnimation
+						texta="Welcome to my portfolio."
+						textb="You can call me Guilherme!"
+					/>
+				),
+			},
+			{
+				path: "/about",
+				element: <AboutMe />,
+			},
+		],
 	},
-	{
-		path: "about",
-		element: <AboutMe/>
-	}
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import styles from "./index.module.css";
+import NavBar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+import { Outlet } from "react-router-dom";
+
+function Home() {
+	return (
+		<div className={styles.home}>
+			<NavBar />
+			<Outlet/>
+			<Footer/>
+		</div>
+	);
+}
+
+export default Home;
