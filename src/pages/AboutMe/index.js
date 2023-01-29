422a028672a7d3d4e44d8dc7d1bf0113308c63e4
Guilherme Oliveira <gui.soliveiras@gmail.com>
2023-01-29 18:45:23 -0300

    Page: AboutMe. Criando o json a ser impresso na tela

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,16 +1,62 @@
-import React from 'react'
+import React from "react";
+import styles from "./index.module.css";
 
 function AboutMe() {
+	const me = {
+		name: "Guilherme Souza de Oliveira",
+		birthday: "15/02/1995",
+		education: [
+			{
+				institution: "Federal University of São Carlos",
+				course: "mathematics",
+				degree: "Bachelor",
+			},
+			{
+				institution: "Federal University of São Carlos",
+				course: "mathematics",
+				degree: "Bachelor",
+			},
+		],
+	};
+	let meString = JSON.stringify(me, null, "\t");
+	console.log(meString.split("\t"));
 
-  const me = {
-    name: 'Guilherme Souza de Oliveira',
-    birthday: '15/02/1995',
-  }
-  let meString = JSON.stringify(me,null,`<br/>`);
-  console.log(meString);
-  return (
-    <div>var me = {meString}</div>
-  )
+	return (
+		<div>
+			<p>var me = {meString.slice(0, 1)}</p>
+			<br />
+			<p className={styles.jsonP}>{meString.split("\t")[1]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[2]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[3]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[5]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[8]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[11]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[14]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[16]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[18]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[21]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[24]}</p>
+			<p className={styles.jsonP}>{meString.split("\t")[27]}</p>
+
+			<p className={styles.jsonP}>{meString.split("\t")[29]}</p>
+
+			<p className={styles.jsonP}>
+				{meString.split("\t")[17].replace("}", "")}
+			</p>
+
+			<p>{meString[meString.lastIndexOf("}")]}</p>
+		</div>
+	);
 }
 
-export default AboutMe
+export default AboutMe;
