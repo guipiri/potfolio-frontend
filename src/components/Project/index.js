28e4c6a93d64a94cc802a960c96271eb5f666d3d
Guilherme Oliveira <gui.soliveiras@gmail.com>
2023-02-03 15:47:55 -0300

    criando o componente project

---
+++ src/components/Project/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styles from "./index.module.css";
+
+
+function Project() {
+  return (
+		<div className={styles.projectConteiner}>
+			<h1>Project 1</h1>
+			<p>
+				Uma breve descrição do projeto aqui! Explica com poucas pvras
+				a motivação do projeto.
+			</p>
+		</div>
+  );
+}
+
+export default Project
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,37 +1,16 @@
 import React from "react";
+import Project from "../../components/Project";
 import styles from "./index.module.css";
 
 function Projects() {
 	return (
 		<div className={styles.projectsPage}>
-			<div className={styles.projectConteiner}>
-				<h1>Project 1</h1>
-				<p>
-					Uma breve descrição do projeto aqui! Explica com poucas
-					palavras a motivação do projeto.
-				</p>
-			</div>
-			<div className={styles.projectConteiner}>
-				<h1>Project 1</h1>
-				<p>
-					Uma breve descrição do projeto aqui! Explica com poucas
-					palavras a motivação do projeto.
-				</p>
-			</div>
-			<div className={styles.projectConteiner}>
-				<h1>Project 1</h1>
-				<p>
-					Uma breve descrição do projeto aqui! Explica com poucas
-					palavras a motivação do projeto.
-				</p>
-			</div>
-			<div className={styles.projectConteiner}>
-				<h1>Project 1</h1>
-				<p>
-					Uma breve descrição do projeto aqui! Explica com poucas
-					palavras a motivação do projeto.
-				</p>
-			</div>
+			<Project />
+			<Project />
+			<Project />
+			<Project />	
+			<Project />	
+			<Project />	
 		</div>
 	);
 }
