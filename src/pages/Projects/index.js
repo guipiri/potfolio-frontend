f2ffc778b404455d67b3a1162eccc436daf495f6
Guilherme Oliveira <gui.soliveiras@gmail.com>
2023-02-03 15:38:14 -0300

    Conteiner de projetos criado e estilizado. Barra de rolagem aplicada para todas as rotas.

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -3,7 +3,28 @@ import styles from "./index.module.css";
 
 function Projects() {
 	return (
-		<div>
+		<div className={styles.projectsPage}>
+			<div className={styles.projectConteiner}>
+				<h1>Project 1</h1>
+				<p>
+					Uma breve descrição do projeto aqui! Explica com poucas
+					palavras a motivação do projeto.
+				</p>
+			</div>
+			<div className={styles.projectConteiner}>
+				<h1>Project 1</h1>
+				<p>
+					Uma breve descrição do projeto aqui! Explica com poucas
+					palavras a motivação do projeto.
+				</p>
+			</div>
+			<div className={styles.projectConteiner}>
+				<h1>Project 1</h1>
+				<p>
+					Uma breve descrição do projeto aqui! Explica com poucas
+					palavras a motivação do projeto.
+				</p>
+			</div>
 			<div className={styles.projectConteiner}>
 				<h1>Project 1</h1>
 				<p>
